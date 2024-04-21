6a7053484ebc427767f9c08703f4dc28e7214bef
Esma K <123084886+krasniqiesma@users.noreply.github.com>
2024-04-21 21:51:57 +0200

    app-rounting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'views'},
+  {path: '', loadChildren: () => import('./views/views.module').then(m => m.ViewsModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
