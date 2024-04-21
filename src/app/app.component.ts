316502f07da8b206da550163b5bcf487e698a105
Esma K <123084886+krasniqiesma@users.noreply.github.com>
2024-04-21 21:57:04 +0200

    preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fitnessProject';
+export class AppComponent implements OnInit {
+  showLoader: boolean = true;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.hideLoader();
+    }, 1000);
+  }
+
+  hideLoader(): void {
+    const progressBar = document.getElementById('progressBar');
+
+    gsap.to(progressBar, {
+      width: '100%',
+      duration: 3,
+      ease: 'power4.out',
+      onComplete: () => {
+        gsap.to(progressBar, {
+          opacity: 0,
+          duration: 0.5,
+          onComplete: () => {
+            this.showLoader = false;
+          }
+        });
+      }
+    });
+  }
+
 }
+
+
